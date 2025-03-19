a32b4311271ca59024dbb777952b35724d9128b0
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-03-19 16:30:12 +0530

    Getting started with SQL Server Stored Procedures

---
+++ Stored Procedure Output Parameters.sql
@@ -0,0 +1,22 @@
+USE BikeStores;
+
+
+--Creating output parameters
+CREATE PROC uspFindProductByModel 
+(
+	@model_year SMALLINT,
+	@product_count INT OUTPUT
+)
+AS
+BEGIN
+SELECT product_name, list_price
+FROM production.products
+WHERE model_year = @model_year;
+SELECT @product_count = @@ROWCOUNT;
+END;
+
+--Calling stored procedures with output parameters
+DECLARE @count INT;
+EXEC uspFindProductByModel @model_year = 2018, @product_count = @count OUTPUT;
+
+SELECT @count AS 'Number of products found';
